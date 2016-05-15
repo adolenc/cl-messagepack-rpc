9818d96d55b0426f5af0cc584a8f9bed0235cf0a
adolenc <andrej.dolenc@student.uni-lj.si>
2016-05-15 15:54:02 +0200

    Copy tests from official github repo
    Tests taken from https://github.com/msgpack-rpc/msgpack-rpc-python/
    In order to run the tests, you need to first run server.py and then
    evaluate
    
      * (asdf:operate 'asdf:test-op :cl-messagepack-rpc)
    
    from your Lisp REPL.

--- t/general.lisp
+++ t/general.lisp
@@ -5,30 +5,50 @@
 
 (defparameter *client* NIL)
 (defparameter *connections* '(
-                               (:file "/tmp/nvim")
-                              ;(:host "127.0.0.1" :port 9900)
+                              ;(:file "/tmp/rpc-test-server")
+                               (:host "127.0.0.1" :port 18800)
                               ;()
                              ))
 
 (def-fixture mrpc-client (connections)
   (dolist (connection connections)
     (let ((*client* (apply #'make-instance 'mrpc:session connection)))
-      (mrpc:call *client* "vim_input" "o<esc>")
       (&body))))
 
+(defmacro test-client (name &body body)
+  `(test ,name
+     (with-fixture mrpc-client (*connections*)
+       ,@body)))
 
-(test sample 
-  (with-fixture mrpc-client (*connections*)
-    (mrpc:call *client* "vim_set_current_line" "hello world!")
-    (is (string= "hello world!" (mrpc:call *client* "vim_get_current_line")))))
+(test-client call
+  (is (string= "world" (mrpc:call *client* "hello")))
+  (is (= 3 (mrpc:call *client* "sum" 1 2)))  
+  (is (null (mrpc:call *client* "nil"))))
 
-(test sample2
-  (with-fixture mrpc-client (*connections*)
-    (mrpc:call *client* "vim_set_current_line" "hello world!")
-    (is (string= "hello world!" (mrpc:call *client* "vim_get_current_line")))))
+(test-client call-async
+  (let ((future1 (mrpc:call-async *client* "hello"))
+        (future2 (mrpc:call-async *client* "sum" 1 2))
+        (future3 (mrpc:call-async *client* "nil")))
+    (is (string= "world" (mrpc:join future1)))
+    (is (= 3 (mrpc:join future2)))
+    (is (null (mrpc:join future3)))))
 
-(test stress-test
+(test-client notify
+  (mrpc:call *client* "clear_register")
+  (mrpc:notify *client* "push_to_register" 1)
+  (mrpc:notify *client* "push_to_register" "test")
+  (mrpc:notify *client* "push_to_register" NIL)
+  (is (equal '(1 "test" NIL T) (mrpc:call *client* "push_to_register" T))))
+
+(test-client raise-error
+  (signals simple-error (mrpc:call *client* "raise_error")))
+
+(test-client unknown-method
+  (signals simple-error (mrpc:call *client* "unknown" T))
+  (handler-case (progn (mrpc:call *client* "unknown" T)
+                       (fail))
+    (error (e) (is (string= "'unknown' method not found" (format NIL "~A" e))))))
+
+(test async-result
   (with-fixture mrpc-client (*connections*)
-    (dotimes (i 100)
-      (mrpc:call *client* "vim_set_current_line" (format NIL "~A" i))
-      (is (string= (format NIL "~A" i) (mrpc:call *client* "vim_get_current_line"))))))
+    (is (string= "You are async!" (mrpc:call *client* "async_result")))))
