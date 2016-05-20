d54a5f4d05a1711d2141cca8f3865800db5a4414
adolenc <andrej.dolenc@student.uni-lj.si>
2016-05-20 23:31:42 +0200

    Add tests for handling requests

--- cl-messagepack-rpc-tests.asd
+++ cl-messagepack-rpc-tests.asd
@@ -3,13 +3,12 @@
   :author "Andrej Dolenc <andrej.dolenc@student.uni-lj.si>"
   :license "MIT"
   :depends-on (#:cl-messagepack-rpc
-               #:fiveam
-               #:flexi-streams)
+               #:fiveam)
   :serial t
   :components ((:file "t/package")
                (:file "t/setup")
-               (:file "t/server")
-               (:file "t/client"))
+               (:file "t/client")
+               (:file "t/server"))
   :perform (test-op (op c)
              (funcall (intern (symbol-name :run!) (find-package :fiveam))
                       (intern (symbol-name :messagepack-rpc-test-suite) (find-package :messagepack-rpc-tests)))))
---
+++ t/server.lisp
@@ -0,0 +1,49 @@
+(in-package :messagepack-rpc-tests)
+(in-suite messagepack-rpc-test-suite)
+
+
+;; We don't have server implemented yet, but we support request/notifcation
+;; handlers for client, so let's simulate fake requests and check client's
+;; responses.
+
+(defmacro simulate-request ((id method &rest params) &body preparations)
+  "Simulate a simple request to *client*, which can be pre-prepared, and return
+*client*'s decoded response."
+  (let ((in (gensym))
+        (out (gensym))
+        (response (gensym)))
+    `(let* ((,in (flex:make-in-memory-input-stream (mrpc::make-request ,id ,method ',params)))
+            (,response (flex:with-output-to-sequence (,out)
+                        (let ((*client* (make-instance 'mrpc:client :input-stream ,in :output-stream ,out)))
+                          ,@preparations
+                          (mrpc::run-once (mrpc::event-loop *client*)))))
+            (mpk::*decoder-prefers-lists* T))
+       (mrpc::parse-response (mpk:decode ,response)))))
+
+(test on-request
+  (let ((sum #'(lambda (a b &rest ns) (apply #'+ a b ns))))
+    (is (equal '(:id 0 :error NIL :result 6)
+               (simulate-request (0 "add" 1 2 3)
+                 (mrpc:register-callback *client* "add" sum))))))
+
+(test on-invalid-request
+  (let ((sum #'(lambda (a b &rest ns) (apply #'+ a b ns)))) ; slightly silly sum to simulate argument errors
+    (is (equal '(:id 0 :error "'add' method not found" :result NIL)
+               (simulate-request (0 "add" 1 2 3))))
+    (is (equal '(:id 1 :error "'add' method: invalid number of arguments: 1" :result NIL)
+               (simulate-request (1 "add" 1)
+                 (mrpc:register-callback *client* "add" sum))))
+    (is (equal '(:id 2 :error "'add' method not found" :result NIL)
+               (simulate-request (2 "add" 1 2 3)
+                 (mrpc:register-callback *client* "add" sum)
+                 (mrpc:remove-callback *client* "add"))))))
+
+(test on-error-in-callback
+  (let ((error-sum #'(lambda () (error "I am throwing an error!")))
+        (warning-sum #'(lambda () (warn "I am throwing a warning!"))))
+    (is (equal '(:id 0 :error "'add' method: I am throwing an error!" :result NIL)
+               (simulate-request (0 "add")
+                 (mrpc:register-callback *client* "add" error-sum))))  
+    (is (equal '(:id 1 :error "'add' method: I am throwing a warning!" :result NIL)
+               (simulate-request (1 "add")
+                 (mrpc:register-callback *client* "add" warning-sum))))))
